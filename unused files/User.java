f08a5ae3da68147b69a1e8acf413ce863036a014
onyangojerry <jerry.onyango.rawlings@gmail.com>
2024-05-13 17:02:25 -0700

    login page

---
+++ unused files/User.java
@@ -0,0 +1,63 @@
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+package com.example.demo.model;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+
+@Document(collection = "users")
+//@Entity
+public class User {
+
+    @Id
+    private long id;
+    private String username;
+    private String password;
+    private String role = "USER"; // default role
+   // private String passwordHash; // stores password hashed
+
+
+    // getters and setters
+    public long getId{
+        return id;
+    }
+
+    public void setId(long id){
+        this.id = id;
+    }
+
+    public String getUsername(){
+        return username;
+    }
+
+    public void setUsername(String username){
+        this.username = username;
+    }
+
+    public String getPassword(){
+        return password;
+    }
+
+    public void setPassword(String password){
+        this.password = password;
+    }
+
+    public String getRole(){
+        return role;
+    }
+
+    public void setRole(String role){
+        this.role = role;
+    }
+
+//    public String getPasswordHash(){
+//        return passwordHash;
+//    }
+
+//    public void setPasswordHash(String passwordHash){
+//        this.passwordHash = passwordHash;
+//    }
+
+}
---
+++ unused files/UserRepository.java
@@ -0,0 +1,14 @@
+import org.springframework.data.repository.CrudRepository;
+
+public interface UserRepository extends CrudRepository<User, Long> {
+    User findByUsername(String username);
+}
+
+package com.example.demo.repository;
+
+import com.example.demo.model.User;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface UserRepository extends MongoRepository<User, String> {
+    User findByUsername(String username);
+}
